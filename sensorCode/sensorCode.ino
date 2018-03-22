8c2ab703b64727cdae3d3db16aabf070e8083913
David Goedicke <da.goedicke@gmail.com>
2018-03-21 20:42:28 -0400

    updating

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,17 +1,26 @@
 #define SENSORPINA A0 // x axis
-#define SENSORPINB A1 // y axis
+ //TODO: define other sensor inputs
 
-unsigned long targetTime=0;       // timing variables
-const unsigned long intervall=50; // timing variables
+unsigned long targetTime=0;
+const unsigned long interval=2500; //TODO: How was should we read
 void setup(){
-Serial.begin(115200);
+// TODO: begin the serial connection with a baudrate of 115200
 }
 
 
 void loop(){
 	if(millis()>=targetTime){
-		targetTime= millis()+intervall;
-		Serial.println(String(analogRead(SENSORPINA))+','+String(analogRead(SENSORPINB)));
+		targetTime= millis()+interval;
+		Serial.println(analogRead(SENSORPINA));
+
+		 //TODO: Add other sensor read outs
+     //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
+		 //TODO: combine them into a string that can be understood by server.js
+		 //TODO: send the string over serial
+
+
 	}
-	// TODO: Add reset code on another sensor
+	// TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
+  // TODO: write the reset message(see server.js) to the serial port
+
 }
